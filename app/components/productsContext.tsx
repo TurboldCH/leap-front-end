20e88846728e15c9f7d337ee89e3d17da548c20d
Turbold Chuluunbaatar <turboch@Turbolds-MacBook-Pro.local>
2024-05-09 09:47:16 -0500

    Fixed ItemDetails

--- app/components/productsContext.tsx
+++ app/components/productsContext.tsx
@@ -5,7 +5,7 @@ import UseLocalStorageState from "../useLocalStorageState/useLocalStorageState";
 
 export const ProductsContext = createContext({
   selectedProducts: [],
-  setSelectedProducts: [],
+  setSelectedProducts: (any: any) => {},
 });
 
 export function ProductsContextProvider({
